dbe03b77e1e8bf7f3efbc82bffe1a9acafa3808c
taegoon <hantaegune@gmail.com>
2017-03-08 10:17:02 +0900

    Add Junit test code

--- Spring/src/com/spring/user/UserDaoTest.java
+++ Spring/src/com/spring/user/UserDaoTest.java
@@ -1,50 +1,41 @@
 package com.spring.user;
 
+import static org.hamcrest.CoreMatchers.is;
+import static org.junit.Assert.assertThat;
 import java.sql.SQLException;
-
+import org.junit.Test;
+import org.junit.runner.JUnitCore;
 import org.springframework.context.ApplicationContext;
-import org.springframework.context.annotation.AnnotationConfigApplicationContext;
 import org.springframework.context.support.ClassPathXmlApplicationContext;
-import org.springframework.context.support.GenericXmlApplicationContext;
 
 public class UserDaoTest {
+	
 
-	public static void main(String[] args) throws ClassNotFoundException, SQLException {
+	@Test
+	public void addAndGet() throws SQLException, ClassNotFoundException {
 		// TODO Auto-generated method stub
-
-//		UserDao dao = new UserDao();
-//		NUserDao dao =new NUserDao();
-//		UserDao dao = new DaoFactory().userDao();
-
-//		ApplicationContext context = new AnnotationConfigApplicationContext(DaoFactory.class);
+		
 		ApplicationContext context = new ClassPathXmlApplicationContext("applicationContext.xml",UserDao.class);
 		
 		UserDao dao = context.getBean("userDao",UserDao.class);
 		
 		User user =new User();
-		user.setId("whiteship");
-		user.setName("조민함");
-		user.setPassword("chominham");
+		user.setId("whiteshop");
+		user.setName("한태군");
+		user.setPassword("hantaegoon");
 		dao.add(user);
 		
 		System.out.println(user.getId() + "등록 성공");
 		
 		User user2 = dao.get(user.getId());
 		
-//		System.out.println(user2.getName());
-//		System.out.println(user2.getPassword());
-//		System.out.println(user2.getId() + "조회 성공");
-		
-		if (!user.getName().equals(user2.getName())) {
-			System.out.println("테스트 실패(name)");
-		}
-		else if(!user.getPassword().equals(user2.getPassword())){
-			System.out.println("테스트 실패(password");
-		}
-		else{
-			System.out.println("조회 테스트 성공");
-		}
+		assertThat(user2.getName(),is(user.getName()));
+		assertThat(user2.getPassword(),is(user.getPassword()));
 		
 	}
+	public static void main(String[] args){
+		JUnitCore.main("com.spring.user.UserDaoTest");
+	}
+	
 
 }
