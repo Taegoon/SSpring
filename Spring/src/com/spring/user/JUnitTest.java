87ffd833298a04c8c8aee0c51e9092704b47bef8
taegoon <hantaegune@gmail.com>
2017-03-09 11:27:25 +0900

    Test JUnit Class

---
+++ Spring/src/com/spring/user/JUnitTest.java
@@ -0,0 +1,29 @@
+package com.spring.user;
+
+import static org.hamcrest.CoreMatchers.is;
+import static org.hamcrest.CoreMatchers.not;
+import static org.hamcrest.CoreMatchers.sameInstance;
+import static org.junit.matchers.JUnitMatchers.hasItem; 
+import static org.junit.Assert.assertThat;
+
+import java.util.HashSet;
+import java.util.Set;
+
+import org.junit.Test;
+
+public class JUnitTest {
+	
+	static Set<JUnitTest> testObjects = new HashSet<JUnitTest>(); 
+	@Test public void test1() { 
+		assertThat(testObjects, not(hasItem(this))); 
+		testObjects.add(this);  
+	}
+	@Test public void test2() {
+		assertThat(testObjects, not(hasItem(this))); 
+		testObjects.add(this);  
+	}
+	@ Test public void test3(){
+		assertThat(testObjects, not(hasItem(this))); 
+		testObjects.add(this);  
+	}
+}
